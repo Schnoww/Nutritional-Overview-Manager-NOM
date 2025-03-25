37edd97677fdf077b7e7fe47c5f4a756f4d70022
Jonah <jegeorge00@gmail.com>
2025-03-25 16:16:46 -0400

    Init Commit

--- src/components/dining/DiningSection.jsx
+++ src/components/dining/DiningSection.jsx
@@ -2,8 +2,7 @@ import { motion } from "framer-motion";
 
 const DiningSection = ({ icon: Icon, title, children }) => {
 	return (
-		<motion.div
-			className='bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-lg shadow-lg rounded-xl p-6 border border-gray-700 mb-8'
+		<motion.div className='bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-lg shadow-lg rounded-xl p-6 border border-gray-700 mb-8'
 			initial={{ opacity: 0, y: 20 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ duration: 0.5 }}
